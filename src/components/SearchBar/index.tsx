5d7c0454f374bd0102d28095dc950631b0e76d7a
罗强 <lq9328@126.com>
2020-12-29 13:10:45 +0800

    feat: 首页

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,35 +1,63 @@
 import React from 'react';
-import {View, TextInput, StyleProp, ViewStyle, StyleSheet} from 'react-native';
+import {
+  View,
+  TextInput,
+  StyleProp,
+  ViewStyle,
+  StyleSheet,
+  GestureResponderEvent,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 interface Props {
   placeholder?: string;
   style?: StyleProp<ViewStyle>;
   disabled?: boolean;
+  onPress?: ((event: GestureResponderEvent) => void) | undefined;
 }
 const SearchBar: React.FC<Props> = (props) => {
-  const {placeholder, style, disabled = false} = props;
+  const {placeholder, style, disabled = false, onPress} = props;
+  const color = '#909399';
   return (
-    <View style={[styles.container, disabled && styles.disabled, style]}>
-      <TextInput style={styles.input} placeholder={placeholder} />
-    </View>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <TouchableWithoutFeedback onPress={onPress}>
+        <View style={[styles.container, style]}>
+          <Icon name="search1" size={16} color={color} />
+          <TextInput
+            style={[styles.input, {color}]}
+            placeholder={placeholder}
+            editable={disabled}
+            enablesReturnKeyAutomatically={true}
+            returnKeyType="search"
+            autoCapitalize="none"
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    height: 34,
-    borderRadius: 17,
+    height: 32,
+    borderRadius: 16,
     overflow: 'hidden',
-    backgroundColor: '#fff',
-  },
-  disabled: {
-    backgroundColor: '#ccc',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: 'rgb(242, 242, 242)',
   },
   input: {
+    flex: 1,
     width: '100%',
     height: '100%',
-    fontSize: 14,
-    color: '#444',
+    fontSize: 13,
+    padding: 0,
+    marginLeft: 6,
   },
 });
 
--- src/pages/chat/recent.tsx
+++ src/pages/chat/recent.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import {View, StyleSheet, StatusBar, Text} from 'react-native';
+import {View, StyleSheet, StatusBar, Text, Image} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/AntDesign';
 import SearchBar from '../../components/SearchBar';
+import color from '../../common/color';
 
 const Recent: React.FC<{}> = () => {
   const navigation = useNavigation();
+  const list = [
+    {
+      id: 1,
+      unread_number: 1,
+      friend_info: {
+        avatar: 'https://im.wangcai.me/speedy_avatar_5.jpg',
+        nickname: '小白',
+      },
+      last_message: {
+        content: '你好啊，我是小白',
+        time: '2小时前',
+      },
+    },
+    {
+      id: 2,
+      unread_number: 1,
+      friend_info: {
+        avatar: 'https://im.wangcai.me/speedy_avatar_2.jpg',
+        nickname: '小白',
+      },
+      last_message: {
+        content: '你好啊，我是小白,你好啊，我是小白,你好啊，我是小白,你好啊，我是小白',
+        time: '2小时前',
+      },
+    },
+  ];
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
@@ -14,33 +42,37 @@ const Recent: React.FC<{}> = () => {
           <Text style={styles.appNameText}>快聊</Text>
         </View>
         <View style={styles.appOperate}>
-          <Text>aa</Text>
-          {/* <u-icon name="plus-circle" size="50" @click="showMenu" /> */}
+          <Icon name="pluscircleo" size={20} color="#333" />
         </View>
       </View>
       <View style={styles.main}>
-        <SearchBar placeholder="请输入关键字" />
-        {/* <u-search height="70" :show-action="false" :disabled="true" /> */}
+        <SearchBar placeholder="请输入关键字" disabled />
         <View style={styles.list}>
-          <View style={styles.listItem}>
-            <View style={styles.avatar}>
-              {/* <image :src="item.friend_info.avatar" style={styles.avatarImage}> */}
-              {/* <u-badge type="error" :count="item.unread_number" :offset="[-10, -10]" /> */}
-            </View>
-            <View style={styles.wrap}>
-              <View style={styles.content}>
-                <View>
-                  <Text style={styles.userName}>item.friend_info.nickname</Text>
+          {list.map((item) => {
+            return (
+              <View style={styles.listItem} key={item.id}>
+                <View style={styles.avatar}>
+                  <Image source={{uri: item.friend_info.avatar}} style={styles.avatarImage} />
+                  {/* <u-badge type="error" :count="item.unread_number" :offset="[-10, -10]" /> */}
                 </View>
-                <View>
-                  <Text style={styles.message}>item.last_message.content</Text>
+                <View style={styles.wrap}>
+                  <View style={styles.content}>
+                    <View>
+                      <Text style={styles.userName}>{item.friend_info.nickname}</Text>
+                    </View>
+                    <View>
+                      <Text style={styles.message} numberOfLines={1}>
+                        {item.last_message.content}
+                      </Text>
+                    </View>
+                  </View>
+                  <View>
+                    <Text style={styles.time}>{item.last_message.time}</Text>
+                  </View>
                 </View>
               </View>
-              <View>
-                <Text style={styles.time}>item.last_message.time</Text>
-              </View>
-            </View>
-          </View>
+            );
+          })}
         </View>
       </View>
     </SafeAreaView>
@@ -50,13 +82,14 @@ const Recent: React.FC<{}> = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
   },
   header: {
     flexDirection: 'row',
     alignItems: 'center',
     paddingLeft: 15,
     paddingRight: 15,
-    backgroundColor: '#F8F8F8',
+    backgroundColor: '#FFF',
     height: 48,
     borderBottomColor: '#eaeaea',
     borderBottomWidth: 1,
@@ -80,6 +113,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   listItem: {
+    flexDirection: 'row',
     alignItems: 'center',
   },
   avatar: {
@@ -87,6 +121,7 @@ const styles = StyleSheet.create({
     width: 42,
     height: 42,
     marginRight: 12,
+    overflow: 'hidden',
   },
   avatarImage: {
     width: '100%',
@@ -95,7 +130,10 @@ const styles = StyleSheet.create({
   },
   wrap: {
     flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
+    borderBottomColor: color.borderColor,
+    borderBottomWidth: 1,
     paddingTop: 10,
     paddingBottom: 10,
   },
@@ -104,7 +142,8 @@ const styles = StyleSheet.create({
   },
   userName: {
     fontSize: 16,
-    color: '#3333',
+    color: '#333',
+    fontWeight: '600',
   },
   message: {
     fontSize: 13,
