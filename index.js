261fc7048e8bc8914420197c818dcdd19a4af24a
罗强 <lq9328@126.com>
2020-12-31 11:01:08 +0800

    feat: 启动屏

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
+import { name as appName } from './app.json';
 import App from './src/App';
-import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
