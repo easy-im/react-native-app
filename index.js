d487a34e434eb723fdf8ea8694f9c9cf222f13b0
luoqiang <qiang.luo@shopee.com>
2022-05-09 14:07:37 +0800

    feat: 修改app.tsx

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { AppRegistry, LogBox } from 'react-native';
 import { appName } from './app.json';
-import App from './src/app1';
+import App from './src/app';
 
 LogBox.ignoreAllLogs();
 
